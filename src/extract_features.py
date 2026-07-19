cdd25b069563c9484f3babbc72aeea268642d74d
Dennis Dang <dennisdang@Denniss-MacBook-Air.local>
2026-07-18 22:03:47 -0700

    Began sentiment analysis and FinBert setup

---
+++ src/extract_features.py
@@ -0,0 +1,16 @@
+"""
+Runs headlines through a frozen FinBert and saves sentiment features (pos, neg, neu)
+"""
+import pandas as pd
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from src.config import INTERIM_DIR, PROCESSED_DIR
+
+# Load frozen FinBert
+tokenizer = AutoTokenizer.from_pretrained('ProsusAI/finbert')
+model = AutoModelForSequenceClassification.from_pretrained('ProsusAI/finbert')
+model.eval() # Frozen
+
+aligned_headlines = pd.read_parquet(INTERIM_DIR / 'aligned_headlines.parquet')
+headlines = aligned_headlines['headline'].tolist()
+
