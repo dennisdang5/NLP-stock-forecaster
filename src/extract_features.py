6f71d322afc4eb14a14253986a60e680df3a6f16
dennisdang5 <158519240+dennisdang5@users.noreply.github.com>
2026-07-19 23:23:56 -0700

    Finished getting sentiment for each headlines and attaching appropriate probas

--- src/extract_features.py
+++ src/extract_features.py
@@ -14,4 +14,29 @@ model.eval() # Frozen
 aligned_headlines = pd.read_parquet(INTERIM_DIR / 'aligned_headlines.parquet')
 headlines = aligned_headlines['headline'].tolist()
 
+"""
+Returns probability for each headline
+"""
 def get_sentiment(texts, batch_size=32):
+    all_probs = []
+    for i in range(0, len(texts), batch_size):
+        batch = texts[i:i + batch_size]
+        inputs = tokenizer(batch, padding=True, truncation=True, max_length=64, return_tensors='pt') # Max length is 64 because during EDA headline max was 44
+        with torch.no_grad(): # No gradients kept
+            logits = model(**inputs).logits
+        probs = torch.softmax(logits, dim=1) # Turns the scores into probabilities such that they sum up to 1
+        all_probs.append(probs)
+        if i % (batch_size * 10) == 0: # Print every 10 batches
+            print(f'{min(i + batch_size, len(texts))} / {len(texts)}')
+    return torch.cat(all_probs).numpy()
+
+print(f' Running FinBERT on {len(headlines)} headlines')
+features = get_sentiment(headlines)
+
+# Attach features back to the df
+aligned_headlines['finbert_pos'] = features[:, 0]
+aligned_headlines['finbert_neg'] = features[:, 1]
+aligned_headlines['finbert_neu'] = features[:, 2]
+
+# Cache to disk
+aligned_headlines.to_parquet(PROCESSED_DIR / 'headlines_features.parquet')
